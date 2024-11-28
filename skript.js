32de4d9daad46843b5c3da1f254672beb32b60ab
Gatze1010 <mtoepfer@stresemanngymnasium.de>
2024-11-28 11:33:43 +0100

    Add files via upload

---
+++ skript.js
@@ -0,0 +1,16 @@
+let navHTML = 
+'<a href="/index.html">Intro Page</a>' +
+'<a href="/transformation.html">Transformation Therory</a>' +
+'<a href="/transformationgermany.html">Germanys transformation after WW2</a>' ;
+
+if (document.getElementById("nav")) {
+    document.getElementById("nav").innerHTML = navHTML;
+}
+
+let footerHTML = 
+'<a href="/kredits.html">sources?</a>' +
+'<a href="/sourcecode.html">a look into the Source code</a>';
+
+if (document.getElementById("footer")) {
+    document.getElementById("footer").innerHTML = footerHTML;
+}
