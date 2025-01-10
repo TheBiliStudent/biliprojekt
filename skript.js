05e464c1c2cc5271daec49999009b1a6f57817a6
TheBiliStudent <mtoepfer@stresemanngymnasium.de>
2025-01-10 10:54:44 +0100

    Update skript.js

--- skript.js
+++ skript.js
@@ -1,15 +1,15 @@
 let navHTML = 
-'<a href="/index.html">🏠</a>' +
-'<a href="/transformation.html">Transformation Therory</a>' +
-'<a href="/transformationgermany.html">Germanys transformation after WW2</a>' ;
+'<a href="index.html">🏠</a>' +
+'<a href="transformation.html">Transformation Therory</a>' +
+'<a href="transformationgermany.html">Germanys transformation after WW2</a>' ;
 
 if (document.getElementById("nav")) {
     document.getElementById("nav").innerHTML = navHTML;
 }
 
 let footerHTML = 
-'<a href="/kredits.html">sources?</a>' +
-'<a href="/sourcecode.html">a look into the Source code</a>';
+'<a href="kredits.html">sources?</a>' +
+'<a href="sourcecode.html">a look into the Source code</a>';
 
 if (document.getElementById("footer")) {
     document.getElementById("footer").innerHTML = footerHTML;
