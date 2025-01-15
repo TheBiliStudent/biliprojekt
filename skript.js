7c9fe9cbb64f681f49dcf7d9e0aa298a12f4696b
Vince-333 <carlvincent.bergmann@gmail.com>
2025-01-15 20:00:22 +0100

    Update skript.js

--- skript.js
+++ skript.js
@@ -2,8 +2,8 @@ let navHTML =
 '<a href="index.html">⌂</a>' +
 '<a href="transformation.html">Transformation Theory</a>' +
 '<a href="liberalisation.html">Germanys transformation after WW2</a>' +
-'<a href="soundsofchange.html">Sounds Of Change</a>' +
-'<a href="initialposition.html">Initial Position in 1989</a>' +
+'<a href="soundsofchange.html">Sounds of change</a>' +
+'<a href="initialposition.html">Initial position in 1989</a>' +
 '<a href="peacefulrevolution.html">Peaceful revolution in the GDR</a>' ;
 
 if (document.getElementById("nav")) {
